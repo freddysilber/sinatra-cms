7c86e275a128a553d34b071531f5dff3331980a1
Freddy Silber <freddy.silber@cloudcoach.com>
2019-10-17 22:44:44 -0600

    final push. added some the last of the login/out

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -36,4 +36,24 @@ class UsersController < ApplicationController
         end
     end
 
+
+    # post '/login' do
+    #     user = User.find_by(:username => params[:username])
+    #     if user && user.authenticate(params[:password])
+    #       session[:user_id] = user.id
+    #       redirect to "/tweets"
+    #     else
+    #       redirect to '/signup'
+    #     end
+    #   end
+    
+    #   get '/logout' do
+    #     if logged_in?
+    #       session.destroy
+    #       redirect to '/login'
+    #     else
+    #       redirect to '/'
+    #     end
+    #   end
+
 end
