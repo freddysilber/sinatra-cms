298054ffc860b87808e6f967b02d27927ef661fa
Freddy Silber <freddy.silber@cloudcoach.com>
2019-10-17 21:43:30 -0600

    mvc layout for project

--- Gemfile
+++ Gemfile
@@ -7,5 +7,15 @@ gem 'sqlite3'
 gem 'sinatra'
 gem 'tux'
 gem 'pry'
+gem 'thin'
 gem 'rake'
 gem 'shotgun'
+gem 'require_all'
+gem 'bcrypt'
+
+group :test do
+  gem 'rspec'
+  gem 'capybara'
+  gem 'rack-test'
+  gem 'database_cleaner', git: 'https://github.com/bmabey/database_cleaner.git'
+end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,10 +6,25 @@ class UsersController < ApplicationController
     
     get '/signup' do
         if !logged_in?
-            erb :'users/create_user', locals: {message: "Please sign up before you sign in"}
+            erb :'users/create_user', locals: {message: "Create a new account!"}
         else
             redirect to '/tweets'
         end 
+    end
+
+    post '/signup' do
+        if params[:username] == "" || params[:email] == "" || params[:password] == ""
+            redirect to '/signup'
+        else
+            @user = User.new(
+                :username => params[:username], 
+                :email => params[:email], 
+                :password => params[:password]
+            )
+            @user.save
+            session[:user_id] = @user.id
+            redirect to '/tweets'
+        end
     end
 
 end
