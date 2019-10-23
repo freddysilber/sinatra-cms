2f97b6eb7996b1fb3d823292e564e2159d184965
Freddy Silber <freddy.silber@cloudcoach.com>
2019-10-22 20:14:54 -0600

    consistancy and coloring

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -40,7 +40,8 @@ class UsersController < ApplicationController
         user = User.find_by(:username => params[:username])
         if user && user.authenticate(params[:password])
             session[:user_id] = user.id
-            redirect to "/projects"
+            # redirect to "/projects"
+            redirect to "/"
         else
             redirect to '/signup'
         end
